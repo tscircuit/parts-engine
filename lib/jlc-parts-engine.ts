473a4e4602b1b3fe668c978166d22570136c877d
imrishabh18 <belikerishabh@gmail.com>
2025-05-19 21:01:55 +0530

    Add the missing components

--- lib/jlc-parts-engine.ts
+++ lib/jlc-parts-engine.ts
@@ -54,9 +54,9 @@ export const jlcPartsEngine: PartsEngine = {
       sourceComponent.type === "source_component" &&
       sourceComponent.ftype === "simple_pin_header"
     ) {
-      let pitch
+      let pitch: number | undefined
       if (footprinterString?.includes("_p")) {
-        pitch = footprinterString.split("_p")[1]
+        pitch = Number(footprinterString.split("_p")[1])
       }
       const { headers } = await getJlcPartsCached(
         "headers",
@@ -73,6 +73,116 @@ export const jlcPartsEngine: PartsEngine = {
       )
       return {
         jlcpcb: headers.map((h: any) => `C${h.lcsc}`).slice(0, 3),
+      }
+    } else if (
+      sourceComponent.type === "source_component" &&
+      sourceComponent.ftype === "simple_potentiometer"
+    ) {
+      const { potentiometers } = await getJlcPartsCached("potentiometers", {
+        resistance: sourceComponent.max_resistance,
+        package: footprinterString,
+      })
+      return {
+        jlcpcb: potentiometers.map((p: any) => `C${p.lcsc}`).slice(0, 3),
+      }
+    } else if (
+      sourceComponent.type === "source_component" &&
+      sourceComponent.ftype === "simple_diode"
+    ) {
+      const { diodes } = await getJlcPartsCached("diodes", {
+        package: footprinterString,
+      })
+      return {
+        jlcpcb: diodes.map((d: any) => `C${d.lcsc}`).slice(0, 3),
+      }
+    } else if (
+      sourceComponent.type === "source_component" &&
+      sourceComponent.ftype === "simple_chip"
+    ) {
+      const { chips } = await getJlcPartsCached("chips", {
+        package: footprinterString,
+      })
+      return {
+        jlcpcb: chips.map((c: any) => `C${c.lcsc}`).slice(0, 3),
+      }
+    } else if (
+      sourceComponent.type === "source_component" &&
+      sourceComponent.ftype === "simple_transistor"
+    ) {
+      const { transistors } = await getJlcPartsCached("transistors", {
+        package: footprinterString,
+        transistor_type: sourceComponent.transistor_type,
+      })
+      return {
+        jlcpcb: transistors.map((t: any) => `C${t.lcsc}`).slice(0, 3),
+      }
+    } else if (
+      sourceComponent.type === "source_component" &&
+      sourceComponent.ftype === "simple_power_source"
+    ) {
+      const { power_sources } = await getJlcPartsCached("power_sources", {
+        voltage: sourceComponent.voltage,
+        package: footprinterString,
+      })
+      return {
+        jlcpcb: power_sources.map((p: any) => `C${p.lcsc}`).slice(0, 3),
+      }
+    } else if (
+      sourceComponent.type === "source_component" &&
+      sourceComponent.ftype === "simple_inductor"
+    ) {
+      const { inductors } = await getJlcPartsCached("inductors", {
+        inductance: sourceComponent.inductance,
+        package: footprinterString,
+      })
+      return {
+        jlcpcb: inductors.map((i: any) => `C${i.lcsc}`).slice(0, 3),
+      }
+    } else if (
+      sourceComponent.type === "source_component" &&
+      sourceComponent.ftype === "simple_crystal"
+    ) {
+      const { crystals } = await getJlcPartsCached("crystals", {
+        frequency: sourceComponent.frequency,
+        load_capacitance: sourceComponent.load_capacitance,
+        package: footprinterString,
+      })
+      return {
+        jlcpcb: crystals.map((c: any) => `C${c.lcsc}`).slice(0, 3),
+      }
+    } else if (
+      sourceComponent.type === "source_component" &&
+      sourceComponent.ftype === "simple_mosfet"
+    ) {
+      const { mosfets } = await getJlcPartsCached("mosfets", {
+        package: footprinterString,
+        mosfet_mode: sourceComponent.mosfet_mode,
+        channel_type: sourceComponent.channel_type,
+      })
+      return {
+        jlcpcb: mosfets.map((m: any) => `C${m.lcsc}`).slice(0, 3),
+      }
+    } else if (
+      sourceComponent.type === "source_component" &&
+      sourceComponent.ftype === "simple_resonator"
+    ) {
+      const { resonators } = await getJlcPartsCached("resonators", {
+        frequency: sourceComponent.frequency,
+        package: footprinterString,
+      })
+      return {
+        jlcpcb: resonators.map((r: any) => `C${r.lcsc}`).slice(0, 3),
+      }
+    } else if (
+      sourceComponent.type === "source_component" &&
+      sourceComponent.ftype === "simple_switch"
+    ) {
+      const { switches } = await getJlcPartsCached("switches", {
+        switch_type: sourceComponent.type,
+        package: footprinterString,
+      })
+      return {
+        jlcpcb: switches.map((s: any) => `C${s.lcsc}`).slice(0, 3),
       }
     }
     return {}
